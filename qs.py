6f56bc1c637adf56035489eef37edde786d1ef47
Bill <billebeling@gmail.com>
2015-08-11 07:02:51 -0400

    made some changes to quicksort.py

--- qs.py
+++ qs.py
@@ -1,24 +1,25 @@
-from random import randint
+from random import randint, random
 
 # global space is evil, but this lets the functions act as though it
 # were pass-by-reference
-A = [6,2,8,4,0,5,7,9,1,3]
+#A = [6,2,8,4,0,5,7,9,1,3]
+a =50
+A = [int(a * random()) for i in xrange(a)]
+count = 0
+# with open("QuickSort.txt", 'r') as f:
+#     A = [int(x) for x in f.read().split("\r\n") if x ]
 
 def parter(A,L,R):
-    print 'starting {},{}'.format(L,R)
+    print 'starting {}'.format(A[L:R])
+
     pivot = A[L] # leftmost elem in bounds
     K = L + 1
     for J in range(L+1,R+1):
         if A[J] < pivot:
             swap(A, K,J)
             K = K + 1
-
     swap(A,L, K-1)
-
-
-    print A
     return K-1
-
 
 def swap(A,from_idx, to_idx):
     temp = A[to_idx]
@@ -26,18 +27,19 @@ def swap(A,from_idx, to_idx):
     A[from_idx]= temp
     return A
 
-
 def quicksort(A,L,R):
+    part_idx = parter(A,L,R)
 
-        part_idx = parter(A,L,R)
+    print '{}: {}'.format(part_idx, A)
 
-        print '{}:\t{},\t{}'.format(part_idx, A[0:part_idx], A[part_idx+1:R+1])
+    count = count + 1
 
-        if len(A[0:part_idx]) > 1: quicksort(A,0,part_idx)
-        if len(A[part_idx+1:R+1]) > 1: quicksort(A,part_idx+1,R+1)
+    if R > L and count < 10:
+        print '({}, {}) {}: {}'.format(L, R, part_idx, A)
 
-        return A
+        quicksort(A,0,part_idx-1)
+        quicksort(A,part_idx+1,R)
 
 
 if __name__ == '__main__':
-    print quicksort(A,0,9)
+    quicksort(A,0,len(A)-1)
